ee8054e897cfd438477535aab6aeff1ded67235c
Antonio Mauricio <oantoniomauricio@gmail.com>
2023-02-28 18:11:59 +0000

    make slider (re)create the grid

--- scripts.js
+++ scripts.js
@@ -20,32 +20,45 @@ squaresContainer.addEventListener("mouseup", () => {
 })
 
 // loop will create the squares
-let squareArray = [];
 
 let numberOfSquares = 16;
 
-for (let i = 0; i < (numberOfSquares * numberOfSquares); i++) {
-    squareArray.push(`square${i}`);
+function createGrid() {
+
+    //wiil first clean the previous grid
+    let squareArray = [];
+    Array.from(containerChildren).forEach(children => children.remove());
+
+    for (let i = 0; i < (numberOfSquares * numberOfSquares); i++) {
+        squareArray.push(`square${i}`);
+    }
+
+    squareArray.forEach(square => {
+        square = document.createElement("div");
+        square.classList.add('square');
+        square.style.cssText = `width: ${480 / numberOfSquares}px; height: ${480 / numberOfSquares}px`;
+        square.addEventListener("mousemove", () => {
+            if (holdingMouse) { square.classList.add("squareOn") }
+        });
+        square.addEventListener("mousedown", () => {
+            square.classList.toggle("squareOn")
+        })
+        squaresContainer.appendChild(square);
+    })
+
 }
 
-squareArray.forEach(square => {
-    square = document.createElement("div");
-    square.classList.add('square');
-    square.style.cssText = `width: ${480 / numberOfSquares}px; height: ${480 / numberOfSquares}px`;
-    square.addEventListener("mousemove", () => {
-        if (holdingMouse) { square.classList.add("squareOn") }
-    });
-    squaresContainer.appendChild(square);
-})
+createGrid();
 
 //add functionality to the buttons
 clearButton.addEventListener("click", () => { Array.from(containerChildren).forEach(children => children.classList.remove("squareOn")) })
 
 // add functionality to the slider
-sliderValue.innerText = slider.value;
+sliderValue.innerText = `${slider.value} x ${slider.value} Grid`;
 
 slider.oninput = function () {
-    sliderValue.innerText = this.value;
+    sliderValue.innerText = `${this.value} x ${this.value} Grid`;
     numberOfSquares = this.value;
     console.log(numberOfSquares);
+    createGrid();
 }

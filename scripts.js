824763a40fb731fd24a4e49d8a132f370fb9487b
Antonio Mauricio <oantoniomauricio@gmail.com>
2023-02-28 12:47:03 +0000

    add the js loop for the squares creation

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
 // select the html elements
-
 let squaresContainer = document.querySelector("#squaresContainer")
 
-console.log(squaresContainer)
+// loop will create the squares
+let squareArray = [];
+
+let numberOfSquares = 16;
+
+for (let i = 0; i < (numberOfSquares * numberOfSquares); i++) {
+    squareArray.push(`square${i}`);
+}
+
+squareArray.forEach(square => {
+    square = document.createElement("div");
+    square.classList.add('square');
+    square.style.cssText = `width: ${480 / numberOfSquares}px; height: ${480 / numberOfSquares}px`;
+    squaresContainer.appendChild(square);
+})
